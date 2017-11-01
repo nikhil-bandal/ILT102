5e84c3bc6993d4c7ee3db7ac121fed30a6520323
Tushar Ghanti <htu218@mainsrv.htintra.net>
2017-11-01 15:39:11 +0530

    Message

--- push_req.sh
+++ push_req.sh
@@ -1,11 +1,12 @@
 #/bin/bash
-#-- Script to automate https://help.github.com/articles/why-is-git-always-asking-for-my-password
+
+#-- Script to automate https to ssh conversion
 
 REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
 if [ -z "$REPO_URL" ]; then
   echo "-- ERROR:  Could not identify Repo url."
   echo "   It is possible this repo is already using SSH instead of HTTPS."
-  exit
+ # exit
 fi
 
 USER=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
@@ -31,3 +32,8 @@ CHANGE_CMD="git remote set-url origin $NEW_URL"
 `$CHANGE_CMD`
 
 echo "Success"
+
+
+echo "Executing PULL from GIT repo $NEW_URL"
+
+#echo "Pushing the file $1 to GIT repo $2"
